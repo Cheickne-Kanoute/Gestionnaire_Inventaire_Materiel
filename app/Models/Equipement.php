c252e8bfef17ecc44a767d84ee00c0cec2017cfe
Sekououolo <ouologuemsekou12@gmail.com>
2026-07-20 16:25:51 +0000

    configuration de la bd et ORM

---
+++ app/Models/Equipement.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Equipement extends Model
+{
+    //
+    use HasFactory;
+    protected $fillable = [
+        'nom',
+        'type',
+        'adresse_ip',
+        'date_acquisition',
+        'statut'
+    ];
+
+}
